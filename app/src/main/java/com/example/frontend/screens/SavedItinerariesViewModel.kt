8bea6e475394770a6d91d4b36389f7b6633c5fc1
David Cuahonte Cuevas <david.cuahonte-cuevas@algosup.com>
2025-04-08 15:01:07 +0200

    Reworked saved itineraries view model

--- app/src/main/java/com/example/frontend/screens/SavedItinerariesViewModel.kt
+++ app/src/main/java/com/example/frontend/screens/SavedItinerariesViewModel.kt
@@ -2,17 +2,23 @@ package com.example.frontend.screens
 
 import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 
 class SavedItinerariesViewModel : ViewModel() {
-    val savedItineraries = mutableStateListOf<String>()
-
-    fun saveItinerary(itinerary: String) {
+    var savedItineraries = mutableStateListOf<Pair<String, String>>()
+    var nextPlan = mutableStateOf<Pair<String, String>?>(null)
+    fun saveItinerary(itinerary: Pair<String, String>) {
         if (!savedItineraries.contains(itinerary)) {
             savedItineraries.add(itinerary)
         }
     }
 
-    fun removeItinerary(itinerary: String) {
+    fun removeItinerary(itinerary: Pair<String, String>) {
         savedItineraries.remove(itinerary)
+    }
+
+    fun setAsNextPlan(itinerary: Pair<String, String>) {
+        nextPlan.value = itinerary
+        removeItinerary(itinerary)
     }
 }
