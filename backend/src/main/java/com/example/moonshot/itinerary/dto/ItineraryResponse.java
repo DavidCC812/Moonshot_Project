f12051269e0e63b5b73c51103401ae1673ceaae1
David Cuahonte Cuevas <david.cuahonte-cuevas@algosup.com>
2025-05-13 11:25:27 +0200

    Itinerary DTOs

---
+++ backend/src/main/java/com/example/moonshot/itinerary/dto/ItineraryResponse.java
@@ -0,0 +1,22 @@
+package com.example.moonshot.itinerary.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+public class ItineraryResponse {
+    private UUID id;
+    private String title;
+    private String description;
+    private BigDecimal price;
+    private Integer duration;
+    private Float rating;
+    private UUID destinationId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
