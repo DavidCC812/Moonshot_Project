5818085889c240d7ac277bec1f40bec201280eba
David Cuahonte Cuevas <david.cuahonte-cuevas@algosup.com>
2025-05-13 11:38:18 +0200

    instantiated tests

---
+++ backend/src/main/test/java/com/example/moonshot/MoonshotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moonshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonshotProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
